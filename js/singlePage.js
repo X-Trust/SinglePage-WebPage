4fc65b040929d8ce6151fa3da722f0070d29fda7
root <cks_jr@yahoo.com>
2014-11-11 13:30:54 -0500

    Updated repository

---
+++ js/singlePage.js
@@ -0,0 +1,52 @@
+/* Written by: Clayotn Smith
+   File: Single page web app
+   
+*/
+
+(function(){
+  
+  // Bind an event to window.onhashchange that, when the hash changes, gets the
+    // hash and adds the class "selected" to any matching nav link.
+    function loadContent( fileName ){
+	$.get( "content/" + fileName + '.html')
+	    .success(function(data) {
+		$('#anchor').html(data);
+	    })
+	    .error(function() {
+		$('#anchor').html('<h1 class=centeredContent> page ' + fileName + ' missing </h1>');    });
+    };
+    
+    
+    // A function that handles the hashchange event
+    function nav() {
+	
+	// get the hash
+	var hash = location.hash.substr(1);
+	
+	// Log the new hash.
+	console.log( 'The hash is ' + ( hash.replace( /^#/, '' ) || 'blank' ) + '.') ;
+	
+	// Iterate over all nav links, setting the "selected" class as-appropriate.
+	$('a').removeClass('active');
+	
+	//add the active class to the current link.
+	$('a[href="#' + hash + '"]').addClass('active');
+
+	// Insert the content from respective html file.
+	loadContent(hash);
+    };
+
+    // If there is no hash is the url then the page will default to home
+    if (!location.hash) {
+	location.hash = '#Home'; 
+	nav();
+	
+	//if there is a hash then load that page
+    } else if( $.trim($("#anchor").html()) === '' ) {
+	nav();
+    }
+    
+    // On the hashchange event, call the nav function.
+    $(window).on("hashchange", nav );
+    
+})();
